12b58f3b8dd521a86f2f09ec1e87cd62091951cf
Sophie Gu <sophieqgu@gmail.com>
2021-01-08 05:34:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -20,35 +20,42 @@ function fetchToyCollection() {
   fetch("http://localhost:3000/toys")
   .then(response => response.json())
   .then(json => {
-    const toyCollection = document.getElementById('toy-collection');
-
     for (const toyObject in json.message) {
-      const toyCard = document.createElement('div');
-      toyCard.setAttribute('class', 'card');
-
-      const toyName = document.createElement('h2');
-      toyName.innerText = toyObject.name;
-      toyCard.appendChild(toyName);
-
-      const toyImage = document.createElement('img');
-      toyImage.src = toyObject.image;
-      toyImage.setAttribute('class', 'toy-avatar');
-      toyCard.appendChild(toyImage);
-
-      const toyLikes = document.createElement('p');
-      toyLikes.innerText = `${toyObject.likes} Likes`;
-      toyCard.appendChild(toyLikes);
-
-      const likeBtn = document.createElement('button');
-      likeBtn.setAttribute('class', 'like-btn');
-      likeBtn.setAttribute('id', toyObject.id)
-      likeBtn.innerText = 'Like <3';
-      likeBtn.addEventListener('click', event => {
-        likeToy(event)
-      })
-      toyCard.appendChild(likeBtn);
-
-      toyCollection.appendChild(toyCard);
+      renderToy(toyObject);
     }
   })
+}
+
+function renderToy(toyObject) {
+  const toyCard = document.createElement('div');
+  toyCard.setAttribute('class', 'card');
+
+  const toyName = document.createElement('h2');
+  toyName.innerText = toyObject.name;
+  toyCard.appendChild(toyName);
+
+  const toyImage = document.createElement('img');
+  toyImage.src = toyObject.image;
+  toyImage.setAttribute('class', 'toy-avatar');
+  toyCard.appendChild(toyImage);
+
+  const toyLikes = document.createElement('p');
+  toyLikes.innerText = `${toyObject.likes} Likes`;
+  toyCard.appendChild(toyLikes);
+
+  const likeBtn = document.createElement('button');
+  likeBtn.setAttribute('class', 'like-btn');
+  likeBtn.setAttribute('id', toyObject.id)
+  likeBtn.innerText = 'Like <3';
+  likeBtn.addEventListener('click', event => {
+    likeToy(event)
+  })
+  toyCard.appendChild(likeBtn);
+
+  const toyCollection = document.getElementById('toy-collection');
+  toyCollection.appendChild(toyCard);
+}
+
+function addNewToy(event) {
+  
 }
